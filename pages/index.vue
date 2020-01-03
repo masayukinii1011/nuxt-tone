d43e4fb2feab164c7c1e3c4f35b52300f0183d0e
masayukinii1011 <masayukinii1011@gmail.com>
2020-01-03 20:03:26 +0900

    first commit, tone.js追加

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @click="handleClick()" class="container">
+    <span>CLICK!</span>
+  </div>
+</template>
+
+<script>
+import Tone from "tone";
+
+export default {
+  data: function() {
+    return {
+      synth: ""
+    };
+  },
+  mounted: function() {
+    this.synth = new Tone.Synth().toMaster();
+  },
+  methods: {
+    handleClick: function() {
+      this.synth.triggerAttackRelease("C6", "8n");
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+}
+</style>
